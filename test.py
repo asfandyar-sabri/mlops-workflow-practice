78549e7d717873c32c11654b65bdcbd0f45396cf
Sami Ullah Shah <53942723+samiullah6799@users.noreply.github.com>
2023-02-04 10:35:13 +0500

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from wallet import Wallet
 
 wallet_obj = Wallet(10)
 
-def test_total_amount_in_wallet(self):
+def test_total_amount_in_wallet():
   wallet_obj.addAmount(20)
-  self.assertEquals(wallet_obj.getAmount(), 30)
+  assert wallet_obj.getAmount() == 30
