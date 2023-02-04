8f80d6b9be3e1bb5aecc17e3ca65df18d5607a59
Sami Ullah Shah <53942723+samiullah6799@users.noreply.github.com>
2023-02-04 10:30:00 +0500

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,13 +1,14 @@
-
-global balance;
+balance = 0;
 class Wallet:
     def __init__(self, amount):
         balance = amount;
         
     def addAmount(self, amount):
+        global balance
         balance = balance + amount;
     
     def removeAmount(self, amount):
+        global balance
         balance = balance - amount;
         
     def getAmount():
